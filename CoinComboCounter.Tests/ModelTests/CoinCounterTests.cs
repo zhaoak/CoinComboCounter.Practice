371d1735e1328211d0c95eff8f6397e6538f9551
Allie Zhao <allie.k.zhao@gmail.com>
2023-07-12 09:21:22 -0700

    wrote test for coin counter, test successfully fails

--- CoinComboCounter.Tests/ModelTests/CoinCounterTests.cs
+++ CoinComboCounter.Tests/ModelTests/CoinCounterTests.cs
@@ -1,12 +1,26 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CoinComboCounter.Models;
+using System.Collections.Generic;
+using System;
 
 namespace CoinComboCounter.Tests
 {
     [TestClass]
     public class CoinCounterTests
     {
-        CoinCounter testCoinCounter = new CoinCounter();
-        
+        [TestMethod]
+        public void CoinCounter_CountsCoins_Dictionary()
+        {
+            int totalCoinValue = 100;
+            CoinCounter testCoinCounter = new CoinCounter();
+
+            Dictionary<String, int> coinQuantities = new Dictionary<String, int>();
+            coinQuantities.Add("Quarters", 4);
+            coinQuantities.Add("Dimes", 0);
+            coinQuantities.Add("Nickels", 0);
+            coinQuantities.Add("Pennies", 0);
+
+            Assert.AreEqual(coinQuantities, testCoinCounter.FindCoinCountFromValue(totalCoinValue));
+        }
     }
 }
