5349cf93bc7a37955425da3aa19d472bc185eaca
Allie Zhao <allie.k.zhao@gmail.com>
2023-07-12 10:05:33 -0700

    tested full coin counting functionality--works as expected

--- CoinComboCounter.Tests/ModelTests/CoinCounterTests.cs
+++ CoinComboCounter.Tests/ModelTests/CoinCounterTests.cs
@@ -9,7 +9,7 @@ namespace CoinComboCounter.Tests
     public class CoinCounterTests
     {
         [TestMethod]
-        public void CoinCounter_CountsQuarters_Dictionary()
+        public void CoinCounter_CountsQuartersNoRemainder_Dictionary()
         {
             // test input value
             int totalCoinValue = 100;
@@ -25,5 +25,42 @@ namespace CoinComboCounter.Tests
             // test
             CollectionAssert.AreEqual(coinQuantities, testCoinCounter.FindCoinCountFromValue(totalCoinValue));
         }
+
+        [TestMethod]
+        public void CoinCounter_CountsQuartersAndDimes_Dictionary()
+        {
+            // test input value
+            int totalCoinValue = 120;
+            CoinCounter testCoinCounter = new CoinCounter();
+
+            // correct coin quantities
+            Dictionary<String, int> coinQuantities = new Dictionary<String, int>();
+            coinQuantities.Add("Quarters", 4);
+            coinQuantities.Add("Dimes", 2);
+            coinQuantities.Add("Nickels", 0);
+            coinQuantities.Add("Pennies", 0);
+
+            // test
+            CollectionAssert.AreEqual(coinQuantities, testCoinCounter.FindCoinCountFromValue(totalCoinValue));
+        }
+
+        [TestMethod]
+        public void CoinCounter_CountsAllCoins_Dictionary()
+        {
+            // test input value
+            int totalCoinValue = 469;
+            CoinCounter testCoinCounter = new CoinCounter();
+
+            // correct coin quantities
+            Dictionary<String, int> coinQuantities = new Dictionary<String, int>();
+            coinQuantities.Add("Quarters", 18);
+            coinQuantities.Add("Dimes", 1);
+            coinQuantities.Add("Nickels", 1);
+            coinQuantities.Add("Pennies", 4);
+
+            // test
+            CollectionAssert.AreEqual(coinQuantities, testCoinCounter.FindCoinCountFromValue(totalCoinValue));
+        }
     }
+
 }
